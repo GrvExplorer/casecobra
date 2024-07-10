48340e844f9e375e6b57beaf54732b6645566d0d
Gaurav <GrvExplorer@outlook.com>
2024-07-10 20:06:00 +0530

    error fixed

--- src/components/ui/Confetti.tsx
+++ src/components/ui/Confetti.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import Confetti from 'react-dom-confetti'
+// import Confetti from 'react-dom-confetti'
 
 export default function Confetti() {
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
@@ -12,10 +12,10 @@ export default function Confetti() {
         aria-hidden="true"
         className="pointer-events-none absolute inset-0 flex select-none justify-center overflow-hidden"
       >
-        <Confetti
+        {/* <Confetti
           active={showConfetti}
           config={{ elementCount: 200, spread: 90 }}
-        />
+        /> */}
       </div>
     </div>
   );
