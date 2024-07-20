34c9c3287bf01e4cc47ce5baaf11b322ce999a1e
Gaurav <GrvExplorer@outlook.com>
2024-07-20 18:43:07 +0530

    fixed webhook

--- src/app/api/webhooks/payment/route.ts
+++ src/app/api/webhooks/payment/route.ts
@@ -1,45 +1,56 @@
-import { db } from "@/db";
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils";
 
 export const POST = async (req: Request, res: Response) => {
   try {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-      await req.json();
+    // FIXME: webhook request have different formate of request body, headers, signature.
 
-    const details = razorpay_order_id + "|" + razorpay_payment_id;
-    console.log("🚀 ~ file: route.ts:9 ~ POST ~ details:", details)
+    const valid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      req.headers.get("razorpay-signature")!,
+      process.env.RAZORPAY_KEY_SECRET!,
+    );
 
+    // const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    //   await req.json();
 
-    const crypto = require("crypto");
-    const hmac = crypto
-      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-      .update(details.toString());
+    // const details = razorpay_order_id + "|" + razorpay_payment_id;
+    // console.log("🚀 ~ file: route.ts:9 ~ POST ~ details:", details)
 
-    const generated_signature = hmac.digest("hex");
+    // const crypto = require("crypto");
+    // const hmac = crypto
+    //   .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    //   .update(details.toString());
 
-    if (generated_signature === razorpay_signature) {
-      console.log(
-        "🚀 ~ file: route.ts:14 ~ POST ~ order:",
-        generated_signature,
-      );
-      const order = await db.order.update({
-        where: {
-          id: razorpay_order_id,
-        },
-        data: {
-          isPaid: true,
-        },
-      });
+    // const generated_signature = hmac.digest("hex");
 
-      if (!order) {
-        throw new Error("Something went wrong while updating the order at verification stage.");
-      }
+    // if (generated_signature === razorpay_signature) {
+    //   const order = await db.order.update({
+    //     where: {
+    //       id: razorpay_order_id,
+    //     },
+    //     data: {
+    //       isPaid: true,
+    //     },
+    //   });
 
-      // TODO: SEND Mail to the user
+    //   if (!order) {
+    //     throw new Error("Something went wrong while updating the order at verification stage.");
+    //   }
 
-      return new Response('OK', {
+    //   // TODO: SEND Mail to the user
+
+    //   return new Response('OK', {
+    //     status: 200,
+    //   });
+    // }
+
+    if (valid) {
+      console.log("🚀 ~ file: route.ts:49 ~ POST ~ valid:", valid);
+      return new Response("OK", {
         status: 200,
       });
     }
+
     return new Response(`Failed to verify signature`, { status: 500 });
   } catch (error) {
     console.log(error);
