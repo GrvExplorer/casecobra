9592503984166b796fd39f59be83a5282d2b3f87
GrvExplorer <icodelife307@gmail.com>
2025-01-18 01:00:22 +0530

    not redirecting

--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,6 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return     <div className="flex h-screen items-center justify-center">
-  <SignUp />;
+  <SignUp />
 </div>
 }
